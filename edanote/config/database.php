d331f6320d15bd172ca45d809e70e50396aab1eb
sakots <sakots8000@gmail.com>
2026-07-29 18:20:06 +0900

    fix: データベース拡張子

--- edanote/config/database.php
+++ edanote/config/database.php
@@ -3,5 +3,5 @@ declare(strict_types=1);
 
 return [
   // SQLite3 データベースファイルのパスとファイル名
-  'path' => dirname(__DIR__) . '/var/data/edanote.sqlite3',
+  'path' => dirname(__DIR__) . '/var/data/edanote.db',
 ];
